0c1979bc6b0905d6275d63f040385075bf12ad62
OH-JOO-YEONG <brb1111@naver.com>
2024-07-21 23:54:28 +0900

    [Fix] Navbar 로고 수정

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -1,18 +1,23 @@
 'use client';
 
 import Image from 'next/image';
-import ImageMomentLogo from '@/public/assets/image-moment-logo.svg';
+import ImageMomentLogo from '@/public/assets/image-moment-primary-logo.svg';
 import { BellIcon } from 'lucide-react';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <header className="w-full flex items-center h-14">
       <h1 className="sr-only">moment-navbar</h1>
       <div className="w-full my-0 flex px-[30px] justify-between">
-        <div className="flex items-center basis-20 shrink-0"></div>
-        <div className="flex w-[157px] h-[30px] relative cursor-pointer">
+        <Link
+          href={'/'}
+          className="flex w-[157px] h-[30px] relative cursor-pointer ml-[-20px]"
+        >
           <Image fill alt="moment-logo" src={ImageMomentLogo} />
-        </div>
+        </Link>
+
+        <div className="flex items-center basis-20 shrink-0"></div>
 
         <div className="flex items-center basis-20 shrink-0 justify-end">
           <BellIcon className="w-6 h-6 cursor-pointer" />
